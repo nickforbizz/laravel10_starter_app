777d4c23b02dc4316cd6184bfb911ea06231461e
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-03-26 15:51:09 +0300

    customize error pages

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -21,7 +21,7 @@
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex items-center pt-8 sm:justify-start sm:pt-0">
                     <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
-                        @yield('code')
+                        @yield('code') 
                     </div>
 
                     <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
