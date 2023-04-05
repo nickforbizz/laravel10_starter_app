50ec3f065ef421a08d9c4df1eac60c94126dda06
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-04-05 13:52:08 +0300

    post crud

--- app/Http/Controllers/cms/PostCategoryController.php
+++ app/Http/Controllers/cms/PostCategoryController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\cms;
+use App\Http\Controllers\Controller;
 
 use App\Models\PostCategory;
 
--- app/Http/Requests/UpdatePostRequest.php
+++ app/Http/Requests/UpdatePostRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdatePostRequest extends FormRequest
 {
@@ -23,6 +24,10 @@ class UpdatePostRequest extends FormRequest
     {
         $rules = [
             'title' => 'required|min:2',
+            'slug' => [
+                'required',
+                Rule::unique('posts')->ignore($this->post)
+            ]
         ];
 
         return $rules;
