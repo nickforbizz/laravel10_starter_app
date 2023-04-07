392882dd856888a597563b4fdeac7dbe7dbc4da5
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-04-07 14:59:45 +0300

    Assign Role, File Gen

---
+++ app/Http/Controllers/cms/AssignRoleController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\cms;
+
+use App\Http\Controllers\Controller;
+use App\Models\ModelHasRole;
+use App\Http\Requests\StoreModelHasRoleRequest;
+use App\Http\Requests\UpdateModelHasRoleRequest;
+
+class AssignRoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreModelHasRoleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ModelHasRole $modelHasRole)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ModelHasRole $modelHasRole)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateModelHasRoleRequest $request, ModelHasRole $modelHasRole)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ModelHasRole $modelHasRole)
+    {
+        //
+    }
+}
