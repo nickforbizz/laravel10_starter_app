3c36764e41e7e95662faf80a67ef1319748bc1aa
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-04-22 00:09:29 +0300

    ProductCategory Controller

---
+++ app/Http/Controllers/cms/ProductCategoryController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\cms;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
+use App\Http\Requests\StoreProductCategoryRequest;
+use App\Http\Requests\UpdateProductCategoryRequest;
+
+use Illuminate\Http\Request;
+use DataTables;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        // return datatable of the makes available
+        $data = ProductCategory::orderBy('created_at', 'desc')->get();
+        if ($request->ajax()) {
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->editColumn('created_at', function ($row) {
+                    return date_format($row->created_at, 'Y/m/d H:i');
+                })
+                ->addColumn('action', function ($row) {
+                    $btn_edit = $btn_del = null;
+                    if (auth()->user()->hasAnyRole('superadmin|admin|editor') || auth()->id() == $row->created_by) {
+                        $btn_edit = '<a data-toggle="tooltip" 
+                                        href="' . route('productCategories.edit', $row->id) . '" 
+                                        class="btn btn-link btn-primary btn-lg" 
+                                        data-original-title="Edit Record">
+                                    <i class="fa fa-edit"></i>
+                                </a>';
+                    }
+
+                    if (auth()->user()->hasRole('superadmin')) {
+                        $btn_del = '<button type="button" 
+                                    data-toggle="tooltip" 
+                                    title="" 
+                                    class="btn btn-link btn-danger" 
+                                    onclick="delRecord(`' . $row->id . '`, `' . route('productCategories.destroy', $row->id) . '`, `#tb_productCategories`)"
+                                    data-original-title="Remove">
+                                <i class="fa fa-times"></i>
+                            </button>';
+                    }
+                    return $btn_edit . $btn_del;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        // render view
+        return view('cms.productCategories.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('cms.productCategories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductCategoryRequest $request)
+    {
+        ProductCategory::create($request->all());
+        return redirect()->back()->with('success', 'Record Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductCategory $productCategory)
+    {
+        return response()
+            ->json($productCategory, 200, ['JSON_PRETTY_PRINT' => JSON_PRETTY_PRINT]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductCategory $productCategory)
+    {
+        return view('cms.productCategories.create', compact('productCategory'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductCategoryRequest $request, ProductCategory $productCategory)
+    {
+        $productCategory->update($request->all());
+
+        // Redirect the user to the user's profile page
+        return redirect()
+            ->route('productCategories.index')
+            ->with('success', 'Record updated successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductCategory $productCategory)
+    {
+        if ($productCategory->delete()) {
+            return response()->json([
+                'code' => 1,
+                'msg' => 'Record deleted successfully'
+            ], 200, ['JSON_PRETTY_PRINT' => JSON_PRETTY_PRINT]);
+        }
+
+        return response()->json([
+            'code' => -1,
+            'msg' => 'Record did not delete'
+        ], 422, ['JSON_PRETTY_PRINT' => JSON_PRETTY_PRINT]);
+    }
+}
