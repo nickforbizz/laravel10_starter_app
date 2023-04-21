fe0202e53a13358a4930ae6b702cdf410239692a
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-04-22 00:01:19 +0300

    Product Controller

--- app/Http/Controllers/cms/ProductController.php
+++ app/Http/Controllers/cms/ProductController.php
@@ -3,21 +3,16 @@
 namespace App\Http\Controllers\cms;
 
 use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use App\Helpers\GlobalHelper;
-use App\Models\Post;
-use App\Models\PostCategory;
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\ProductCategory;
 
-
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
-
-use DataTables;
-
-class PostController extends Controller
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -25,30 +20,30 @@ class PostController extends Controller
     public function index(Request $request)
     {
         // return datatable of the makes available
-        $data = Post::orderBy('created_at', 'desc')->get();
+        $data = Product::orderBy('created_at', 'desc')->get();
         if ($request->ajax()) {
             return Datatables::of($data)
                 ->addIndexColumn()
                 ->editColumn('created_at', function ($row) {
                     return date_format($row->created_at, 'Y/m/d H:i');
                 })
-                ->editColumn('featured_img', function ($row) {
-                    return '<img class="tb_img" src="' . url('storage/' . $row->featured_img) . '" alt="' . $row->slug . '" data-toggle="popover" data-placement="top" data-content="<img src=' . url('storage/' . $row->featured_img) . ' style=\'max-height: 200px; max-width: 200px;\'>">';
+                ->editColumn('photo', function ($row) {
+                    return '<img class="tb_img" src="' . url('storage/' . $row->photo) . '" alt="' . $row->slug . '" data-toggle="popover" data-placement="top" data-content="<img src=' . url('storage/' . $row->photo) . ' style=\'max-height: 200px; max-width: 200px;\'>">';
                 })
                 ->editColumn('category_id', function ($row) {
-                    return $row->post_category->name;
+                    return $row->product_category->name;
                 })
                 ->editColumn('title', function ($row) {
                     return Str::limit($row->title, 10, '...');
                 })
-                ->editColumn('content', function ($row) {
-                    return Str::limit($row->content, 20, '...');
+                ->editColumn('description', function ($row) {
+                    return Str::limit($row->description, 20, '...');
                 })
                 ->addColumn('action', function ($row) {
                     $btn_edit = $btn_del = null;
                     if (auth()->user()->hasAnyRole('superadmin|admin|editor') || auth()->id() == $row->created_by) {
                         $btn_edit = '<a data-toggle="tooltip" 
-                                    href="' . route('posts.edit', $row->id) . '" 
+                                    href="' . route('products.edit', $row->id) . '" 
                                     class="btn btn-link btn-primary btn-lg" 
                                     data-original-title="Edit Record">
                                 <i class="fa fa-edit"></i>
@@ -60,19 +55,19 @@ class PostController extends Controller
                                     data-toggle="tooltip" 
                                     title="" 
                                     class="btn btn-link btn-danger" 
-                                    onclick="delRecord(`' . $row->id . '`, `' . route('posts.destroy', $row->id) . '`, `#tb_posts`)"
+                                    onclick="delRecord(`' . $row->id . '`, `' . route('products.destroy', $row->id) . '`, `#tb_products`)"
                                     data-original-title="Remove">
                                 <i class="fa fa-times"></i>
                             </button>';
                     }
                     return $btn_edit . $btn_del;
                 })
-                ->rawColumns(['featured_img', 'category_id', 'title', 'content', 'action'])
+                ->rawColumns(['photo', 'category_id', 'title', 'description', 'action'])
                 ->make(true);
         }
 
         // render view
-        return view('cms.posts.index');
+        return view('cms.products.index');
     }
 
     /**
@@ -80,71 +75,71 @@ class PostController extends Controller
      */
     public function create()
     {
-        $post_categories = PostCategory::where('active', 1)->get();
-        return view('cms.posts.create', compact('post_categories'));
+        $product_categories = ProductCategory::where('active', 1)->get();
+        return view('cms.products.create', compact('product_categories'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(StoreProductRequest $request)
     {
         $request = $this->storeFeaturedImg($request);
-        $post = Post::create($request->all());
+        $product = Product::create($request->all());
 
-        if ($post) {
+        if ($product) {
             return redirect()
-                ->route('posts.index')
+                ->route('products.index')
                 ->with('success', 'Record Created Successfully');
         }
-        $post_categories = PostCategory::where('active', 1)->get();
+        $product_categories = ProductCategory::where('active', 1)->get();
         return redirect()
-            ->route('cms.posts.create', compact('post_categories'))
+            ->route('cms.products.create', compact('product_categories'))
             ->with('error', 'Error while creating record');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(Product $product)
     {
-        return $post;
+        return $product;
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Post $post)
+    public function edit(Product $product)
     {
-        $post_categories = PostCategory::where('active', 1)->get();
-        return view('cms.posts.create', compact('post', 'post_categories'));
+        $product_categories = ProductCategory::where('active', 1)->get();
+        return view('cms.products.create', compact('product', 'product_categories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(UpdateProductRequest $request, Product $product)
     {
         $request = $this->storeFeaturedImg($request);
-        $post->update($request->all());
+        $product->update($request->all());
 
-        // Redirect the post to the post's profile page
+        // Redirect the product to the product's profile page
         return redirect()
-            ->route('posts.index')
+            ->route('products.index')
             ->with('success', 'Record updated successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(Product $product)
     {
-        // delete post's profile image if it exists
-        if ($post->featured_img && Storage::disk('public')->exists($post->featured_img)) {
-            Storage::disk('public')->delete($post->featured_img);
+        // delete product's profile image if it exists
+        if ($product->photo && Storage::disk('public')->exists($product->photo)) {
+            Storage::disk('public')->delete($product->photo);
         }
 
-        if ($post->delete()) {
+        if ($product->delete()) {
             return response()->json([
                 'code' => 1,
                 'msg' => 'Record deleted successfully'
@@ -157,20 +152,21 @@ class PostController extends Controller
         ]);
     }
 
-    private function storeFeaturedImg(Request $request, Post $post = null)
+
+    private function storeFeaturedImg(Request $request, Product $product = null)
     {
 
 
         // Store Image
         if ($request->has('featuredimg')) {
             // Delete Image
-            if ($post) {
-                if ($post->featured_img && Storage::disk('public')->exists($post->featured_img)) {
-                    Storage::disk('public')->delete($post->featured_img);
+            if ($product) {
+                if ($product->photo && Storage::disk('public')->exists($product->photo)) {
+                    Storage::disk('public')->delete($product->photo);
                 }
             }
-            $featured_img_filename = GlobalHelper::saveImage($request->file('featuredimg'), 'posts', 'public');
-            $request->request->add(['featured_img' => $featured_img_filename]);
+            $photo_filename = GlobalHelper::saveImage($request->file('featuredimg'), 'products', 'public');
+            $request->request->add(['photo' => $photo_filename]);
         }
 
         return  $request;
